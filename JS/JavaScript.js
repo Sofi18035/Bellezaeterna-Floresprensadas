340b2e1d7d972dbd4f993609aa405890d5673fd6
Sofi18035 <ariglez4525@gmail.com>
2025-11-22 22:24:48 -0800

    Add files via upload

---
+++ JS/JavaScript.js
@@ -0,0 +1,17 @@
+const modal = document.getElementById('modal')
+const openModalBtn = document.getElementById('openModalBtn')
+const closeModalBtn = document.getElementById('closeModalBtn')
+
+openModalBtn.addEventListener('click', function(){
+    modal.style.display = 'flex'
+})
+
+closeModalBtn.addEventListener('click', function(){
+    modal.style.display = 'none'
+})
+
+window.addEventListener('click', function(event){
+    if(event.target == modal){
+        modal.style.display = 'none'
+    }
+})
